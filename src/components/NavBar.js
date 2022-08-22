97a710c6fef49f9464746d068b5462c050f54655
Rodri Berdomas <rodriberdomas@gmail.com>
2022-08-22 20:17:03 -0300

    navbar

---
+++ src/components/NavBar.js
@@ -0,0 +1,15 @@
+
+ export const NavBar = () => {
+    return(
+        <header className="navbar">
+            <h1>Enterfix</h1>
+            <nav>
+                <ul>
+                    <a href="#">Home </a>
+                    <a href="#">About </a>
+                    <a href="#">Contact </a>
+                </ul>
+            </nav>
+        </header>
+    )
+}
